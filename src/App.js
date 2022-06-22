3e3aeca0a8b9364da815ceda8ce9acd1d02734ac
Nguyen Tinh <vantinhnguyen728@gmail.com>
2022-06-22 23:17:18 +0700

    update react app demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
 
+const {
+  useState
+} = require('react')
+
+
 function App() {
+  // ==> Ex 1:
+  // const [counter, setCounter] = useState(1);
+  // const handleCounter = () => {
+  //   setCounter(preState => preState + 1)
+  // }
+
+  // ==> Ex 2:
+  // const [info, setInfo] = useState({
+  //   name: 'Dev Nguyen',
+  //   age: 21,
+  //   address: 'HCM, VN'
+  // })
+  // const handleUpdate = () => {
+  //   setInfo(pre => ({ ...pre, bio: 'Dev nguyen is the best' }))
+  // }
+  // ==> Ex 3:
+  // const gifts = [
+  //   'CPU i9', 'RAM 32GB', 'RGB Keyboard'
+  // ]
+  // const [gift, setGift] = useState('Not have any gift')
+  // const handleUpdateGift = () => setGift(gifts[Math.floor(Math.random() * 3)])
+  // ==> Ex 4:
+
+  const courses = [
+    {
+      id: 1,
+      name: 'NodeJs'
+    },
+    {
+      id: 2,
+      name: 'ReactJs'
+    },
+    {
+      id: 3,
+      name: 'VueJs'
+    },
+  ]
+
+  const [itemChecked, setItemChecked] = useState([])
+
+  const handleSubmit = () => {
+    console.log(itemChecked)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="app" style={{ padding: 32 }}>
+      {courses.map(item => (
+        <div key={item.id}>
+          <input
+            type="checkbox"
+            checked={item.id === itemChecked}
+            onChange={() => setItemChecked(item.id)}
+          /> {item.name}
+        </div>
+      ))}
+
+      < button onClick={handleSubmit} > Submit!</button>
+    </div >
+  )
 }
 
 export default App;
